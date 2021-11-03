cdcaf14ef0a59a8c0ee29850aa5b4748b52a2622
TuomasB73 <tuomasbergholm@gmail.com>
2021-11-03 11:19:40 +0200

    Initial commit

---
+++ app/src/main/java/com/virtualmapdevs/ar_vr_map/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.virtualmapdevs.ar_vr_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
