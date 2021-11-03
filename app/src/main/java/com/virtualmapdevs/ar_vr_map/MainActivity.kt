62433305eec4e8d83b2478c892b8201be761fae8
TuomasB73 <tuomasbergholm@gmail.com>
2021-11-03 12:14:08 +0200

    Fixed

--- app/src/main/java/com/virtualmapdevs/ar_vr_map/MainActivity.kt
+++ app/src/main/java/com/virtualmapdevs/ar_vr_map/MainActivity.kt
@@ -3,7 +3,7 @@ package com.virtualmapdevs.ar_vr_map
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : 1234() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
