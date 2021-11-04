7921a0bd40e1e1d181bf097c7efd609bdc15141d
Rikotee <teerek81@gmail.com>
2021-11-04 15:30:20 +0200

    added QR scanner

---
+++ app/src/main/java/com/virtualmapdevs/ar_vr_map/QRScannerFragment.kt
@@ -0,0 +1,129 @@
+package com.virtualmapdevs.ar_vr_map
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+
+class QRScannerFragment : Fragment() {
+
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        checkPermissions()
+        startScanning()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_q_r_scanner, container, false)
+    }
+
+    private fun startScanning() {
+
+        val scanResultTextView = view?.findViewById<TextView>(R.id.scanResultView)
+
+        // Parameters (default values)
+        val scannerView = view?.findViewById<CodeScannerView>(R.id.scanner_view)
+        codeScanner = scannerView?.let { context?.let { it1 -> CodeScanner(it1, it) } }!!
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            activity?.runOnUiThread {
+                //Toast.makeText(activity, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+                scanResultTextView?.text = it.text
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            activity?.runOnUiThread {
+                Toast.makeText(
+                    activity, "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 123) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(activity, "Camera permission granted", Toast.LENGTH_LONG).show()
+                startScanning()
+            } else {
+                Toast.makeText(activity, "Camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::codeScanner.isInitialized) {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        if (::codeScanner.isInitialized) {
+            codeScanner.releaseResources()
+        }
+        super.onPause()
+    }
+
+    private fun checkPermissions() {
+        if (context?.let {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    android.Manifest.permission.CAMERA
+                )
+            } != PackageManager.PERMISSION_GRANTED
+        ) {
+            activity?.let {
+                ActivityCompat.requestPermissions(
+                    it,
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    0
+                )
+            }
+        }
+    }
+}
