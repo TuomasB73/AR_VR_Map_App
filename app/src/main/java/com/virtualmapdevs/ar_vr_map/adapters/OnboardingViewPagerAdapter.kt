0553c2f98242f6012b0bab11ccc681af75acf354
Rikotee <teerek81@gmail.com>
2021-12-14 11:37:42 +0200

    new onboarding images

--- app/src/main/java/com/virtualmapdevs/ar_vr_map/adapters/OnboardingViewPagerAdapter.kt
+++ app/src/main/java/com/virtualmapdevs/ar_vr_map/adapters/OnboardingViewPagerAdapter.kt
@@ -19,17 +19,17 @@ class OnboardingViewPagerAdapter(
             0 -> OnboardingFragment.newInstance(
                 context.resources.getString(R.string.title_onboarding_1),
                 context.resources.getString(R.string.description_onboarding_1),
-                R.drawable.testlogo
+                R.drawable.onboarding1
             )
             1 -> OnboardingFragment.newInstance(
                 context.resources.getString(R.string.title_onboarding_2),
                 context.resources.getString(R.string.description_onboarding_2),
-                R.drawable.testlogo2
+                R.drawable.onboarding2
             )
             else -> OnboardingFragment.newInstance(
                 context.resources.getString(R.string.title_onboarding_3),
                 context.resources.getString(R.string.description_onboarding_3),
-                R.drawable.testlogo4
+                R.drawable.onboarding3
             )
         }
     }
